51deab5b9b67c756ad816047dafbaf8ba4e2719e
fuauda <fuadtesfaye24@gmail.com>
2025-08-06 20:06:13 +0300

    system-architect.md

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
 <>
-<h1>medical link</h1>
+<h1>COMMUNIT link</h1>
 <Button>hello coders</Button>
 </>
   );
