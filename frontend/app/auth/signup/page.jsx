a2b4b5fea5c3f54ea9e485f94c0c789882bee1a7
fuauda <fuadtesfaye24@gmail.com>
2025-08-07 16:59:33 +0300

    Add TypeScript configuration file for frontend project

---
+++ frontend/app/auth/signup/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MultiStepForm from '../../../components/ui/multi-step-form'
+
+const SignupPage = () => {
+  return (
+    <MultiStepForm />
+  )
+}
+
+export default SignupPage
