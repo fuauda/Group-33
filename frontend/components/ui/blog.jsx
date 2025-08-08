0f5c373d3081c5eb3b7c53164dfcb5dd430ed8c7
Muluken Seifu <muleseifu87@gmail.com>
2025-08-08 14:42:13 +0300

    link "see more" button to /blog page

--- frontend/components/ui/blog.jsx
+++ frontend/components/ui/blog.jsx
@@ -83,7 +83,7 @@ return (
                 </span>
                 <h2 className="text-3xl font-semibold text-blue-950 dark:text-gray-200 md:text-4xl xl:text-5xl leading-tight">From our latest Blog Post</h2>
                 <div className="flex justify-center mt-2">
-                    <Link href="#" className="px-5 py-2.5 rounded-xl bg-gray-100 dark:bg-gray-900 border border-gray-200 dark:border-gray-800 text-blue-600 dark:text-gray-300 flex items-center gap-x-3">
+                    <Link href="/blog" className="px-5 py-2.5 rounded-xl bg-gray-100 dark:bg-gray-900 border border-gray-200 dark:border-gray-800 text-blue-600 dark:text-gray-300 flex items-center gap-x-3">
                         See More
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
