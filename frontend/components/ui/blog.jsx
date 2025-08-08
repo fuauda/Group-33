639ae7f9346daa5c3806a986264ea0cb98932d80
Muluken Seifu <muleseifu87@gmail.com>
2025-08-08 16:12:52 +0300

    make 'read more button work'

--- frontend/components/ui/blog.jsx
+++ frontend/components/ui/blog.jsx
@@ -1,7 +1,8 @@
 import Image from "next/image"
 import Link from "next/link"
+
  
-const BlogCard = ({ cover, author, published_at, title, sumary,}) => {
+const BlogCard = ({ cover, author, published_at, title, sumary, id}) => {
 return (
     <div className="bg-white dark:bg-gray-950 border border-gray-100 dark:border-gray-900 px-px rounded-xl">
         <div className="rounded-[11px] bg-gray-200 dark:bg-gray-800 relative">
@@ -26,7 +27,7 @@ return (
             <p className="text-gray-700 dark:text-gray-300 line-clamp-2">
                 {sumary}
             </p>
-            <Link href="#" className="flex items-center gap-x-2 text-blue-600 dark:text-blue-400">
+            <Link href={`../blog/${id}`} className="flex items-center gap-x-2 text-blue-600 dark:text-blue-400">
                 Read more
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
                     <path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd" />
